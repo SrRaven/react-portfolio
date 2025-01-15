1f224959dca087fc8e393cc1a4e6d258a75037fd
Douglas <87214871+SrRaven@users.noreply.github.com>
2025-01-15 12:08:53 -0300

    Versão final

---
+++ src/constants/index.js
@@ -0,0 +1,64 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Sou um desenvolvedor full-stack, apaixonado por criar aplicações web dinâmicas e eficientes. Com uma base sólida em JavaScript, React, Node.js e MySQL, estou ansioso para aplicar minhas habilidades em projetos reais. Busco oportunidades para contribuir com soluções que proporcionem crescimento aos negócios e experiências excepcionais aos usuários.`;
+
+export const ABOUT_TEXT = `Sou um desenvolvedor full-stack dedicado e versátil, com paixão por criar aplicações web eficientes, intuitivas e seguras. Trabalho com tecnologias como React, Node.js, MySQL e JavaScript, sempre buscando aprender e enfrentar novos desafios. Minha jornada no desenvolvimento web começou com uma curiosidade profunda sobre como as coisas funcionam e evoluiu para um compromisso em entregar soluções. Valorizo o trabalho em equipe e a resolução de problemas complexos. Fora do universo da programação, gosto de explorar novas tecnologias, me manter ativo e expandir meus conhecimentos em projetos pessoais.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    link: "https://srraven.github.io/jogodamemoria/",
+    title: "Jogo da Memória",
+    image: project1,
+    description:
+      "Um jogo simples de memória, onde os jogadores combinam pares de cartas idênticas. Desenvolvido com foco em lógica e interatividade.",
+    technologies: ["HTML", "CSS", "JavaScript", ],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "Um site de portfólio pessoal apresentando projetos, habilidades e informações de contato.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+
+];
+
+export const CONTACT = {
+  address: "Brasil, Bahia, Salvador ",
+  phoneNo: "(71) 99672 8402 ",
+  email: "douglasgalvao285@gmail.com ",
+};
